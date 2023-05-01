b8344fc4bf7cac029f0207b3ff298f1831645400
ssham <sergshamiryan@gmail.com>
2023-05-01 22:36:49 +0400

    commit

--- src/main/java/org/example/mockhito/Stock.java
+++ src/main/java/org/example/mockhito/Stock.java
@@ -1,6 +1,7 @@
 package org.example.mockhito;
 
 
+import java.awt.print.Book;
 import java.util.Optional;
 
 public class Stock {
--- src/test/java/org/example/testDoublesFake/FakeTest.java
+++ src/test/java/org/example/testDoublesFake/FakeTest.java
@@ -1,18 +1,40 @@
 package org.example.testDoublesFake;
 
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 public class FakeTest {
 
     @Test
-    public void testFake(){
+    public void testFake() {
         BookService bookService = new BookService(new FakeBookRepository());
-        bookService.addBook(new Book("one","John Conor",1000, LocalDate.now()));
-        assertEquals(2,bookService.findNumberAllBooks());
+        bookService.addBook(new Book("one", "John Conor", 1000, LocalDate.now()));
+        assertEquals(2, bookService.findNumberAllBooks());
+    }
+
+    @Test
+    public void testFakeWithMockito() {
+        FakeBookRepository fakeBookRepository = mock(FakeBookRepository.class);
+        BookService bookService = new BookService(fakeBookRepository);
+        Book book = new Book("1", "Name", 250, LocalDate.now());
+        Book book2 = new Book("2", "name2", 150, LocalDate.now());
+        List<Book> books = new ArrayList<>();
+        books.add(book);
+        books.add(book2);
+
+        //Stubbing - means predefined answers
+        when(fakeBookRepository.findAll()).thenReturn(books);
+
+        assertEquals(2, bookService.findNumberAllBooks());
+
     }
 
 }
